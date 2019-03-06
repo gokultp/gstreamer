0053a0b1590afb26d2d32064b6ea74d0a081a217
Gokul TP <tp.gokul@gail.com>
2019-03-06 16:39:11 +0530

    reading port from env

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gokultp/gstreamer/internal/dbmodels"
 	"github.com/gokultp/gstreamer/internal/routes"
 )
 
-func main() {
+const envPort = "PORT"
 
+func main() {
+	port := os.Getenv(envPort)
+	if port == "" {
+		port = "8080"
+	}
 	err := dbmodels.InitDBConnection()
 	if err != nil {
 		panic(err)
 	}
 	router := routes.InitRoutes()
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":"+port, router)
 }
